f009cdcc94db3ec0b1cd3b090f8cc71c2eeb0315
ftsukic <ftsukic@gmail.com>
2026-09-05 13:54:17 +0800

    feat(cell): 优化点击态背景

--- packages/ui/src/cell/token.ts
+++ packages/ui/src/cell/token.ts
@@ -3,7 +3,7 @@ import type { AliasToken, CellToken } from '../theme'
 export function getCellToken(token: AliasToken): CellToken {
   return {
     backgroundColor: token.colorBgContainer,
-    activeColor: token.colorFillTertiary,
+    activeColor: token.colorFillSecondary,
     borderColor: token.colorBorderSecondary,
     paddingHorizontal: token.paddingMD,
     paddingMD: token.paddingMD,
